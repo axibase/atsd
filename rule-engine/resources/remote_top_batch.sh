e10ae59fadd6ce7a4b06846fea377719db3277af
Anna <snowswim@mail.ru>
2018-01-15 22:05:20 +0300

    functions-script.md update

--- rule-engine/resources/remote_top_batch.sh
+++ rule-engine/resources/remote_top_batch.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-key_location=${1}
-host=${2}
-user=${3}
-count=${4}
-delay=${5}
-rows_n=${6}
+host=${1}
+user=${2}
+count=${3}
+delay=${4}
+rows_n=${5}
 
 
-ssh -i ${key_location} ${host} top -u ${user} -b -n ${count} -d ${delay} | head -n ${rows_n}
+ssh -i /home/axibase/.ssh/def.key ${host} top -u ${user} -b -n ${count} -d ${delay} | head -n ${rows_n}
