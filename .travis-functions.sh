dbc23bad0e58790cf01e25c32a42c6dfd780901b
Anton Rybochkin <anton.rybochkin@axibase.com>
2018-06-14 18:37:56 +0300

    enable custom markdown linting rules from axibase/docs-util repository

--- .travis-functions.sh
+++ .travis-functions.sh
@@ -42,10 +42,10 @@ function linkcheck {
 function stylecheck {
     if [ "$ENABLE_CHECK" = "true" ]; then
         if [ -z $TRAVIS_PULL_REQUEST_BRANCH ]; then
-            markdownlint .
+            markdownlint -r 'docs-util/linting-rules/*' .
         else
             if [[ -n "$(list_modified_md_files)" ]]; then
-                list_modified_md_files | xargs -d '\n' -n1 markdownlint
+                list_modified_md_files | xargs -d '\n' -n1 markdownlint -r 'docs-util/linting-rules/*' {}
             fi;
         fi
     else
@@ -77,7 +77,8 @@ function generate_yaspeller_dictionary {
 }
 
 function install_checkers {
-    npm install --global --production yaspeller spellchecker-cli markdown-link-check markdownlint-cli remark-cli remark-validate-links
+    npm install --global --production yaspeller spellchecker-cli markdown-link-check remark-cli remark-validate-links git+https://github.com/VeselovAlex/markdownlint-cli.git#custom-rules
+    git clone https://github.com/axibase/docs-util --depth=1
     if [ "$TRAVIS_REPO_SLUG" != "axibase/atsd" ]; then
         wget https://raw.githubusercontent.com/axibase/atsd/master/.spelling -O .spelling-atsd
         awk 'FNR==1{print}1' .spelling-atsd .dictionary | sort -u > .spelling
