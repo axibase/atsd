0a3952b887620125b260ae731769fdad7d45767b
Aleksandr Veselov <alexander.veselov@axibase.com>
2021-02-24 15:33:19 +0300

    Add support of Remark42 comments block (#976)

---
+++ .vuepress/theme/enhanceApp.js
@@ -0,0 +1,17 @@
+export default function ({siteData}) {
+    var remarkConfig = siteData.themeConfig.remark42Config;
+
+    if (remarkConfig) {
+        window.remark_config = remarkConfig;
+
+        (function (c) {
+            for (var i = 0; i < c.length; ++i) {
+                var d = document, s = d.createElement('script');
+                s.src = remarkConfig.host + '/web/' + c[i] + '.js';
+                s.defer = true;
+                s.id = "remark42-load-script";
+                (d.head || d.body).appendChild(s);
+            }
+        })(remarkConfig.components || ["embed"]);
+    }
+}
