522f99c103aab79d8c9d9a63e2ada80a8fb5fb44
Aleksandr Veselov <veselov143@gmail.com>
2021-03-10 18:02:18 +0300

    Add landing prototype

--- .vuepress/theme/Home.vue
+++ .vuepress/theme/Home.vue
@@ -1,24 +1,28 @@
 <template>
-  <div class="home">
-    <div class="hero">
-      <img v-if="data.heroImage" :src="$withBase(data.heroImage)" alt="hero">
-      <h1>{{ data.heroText || $title || 'Hello' }}</h1>
-      <p class="description">
-        {{ data.tagline || $description || 'Welcome to your VuePress site' }}
-      </p>
-      <p class="action" v-if="data.actionText && data.actionLink">
-        <NavLink class="action-button" :item="actionLink"/>
-      </p>
-    </div>
-    <div class="features" v-if="data.features && data.features.length">
-      <div class="feature" v-for="feature in data.features">
-        <h2>{{ feature.title }}</h2>
-        <p>{{ feature.details }}</p>
+  <div class="home-wrapper">
+    <div class="home">
+      <div class="hero-bg"></div>
+      <div class="hero-bg-mask"></div>
+      <div class="hero">
+        <img v-if="data.heroImage" :src="$withBase(data.heroImage)" alt="hero">
+        <h1>{{ data.heroText || $title || 'Hello' }}</h1>
+        <p class="description">
+          {{ data.tagline || $description || 'Welcome to your VuePress site' }}
+        </p>
+        <p class="action" v-if="data.actionText && data.actionLink">
+          <NavLink class="action-button" :item="actionLink"/>
+        </p>
       </div>
-    </div>
-    <Content custom/>
-    <div class="footer" v-if="data.footer">
-      {{ data.footer }}
+      <div class="features" v-if="data.features && data.features.length">
+        <div class="feature" v-for="feature in data.features">
+          <h2>{{ feature.title }}</h2>
+          <p>{{ feature.details }}</p>
+        </div>
+      </div>
+      <Content custom/>
+      <div class="footer" v-if="data.footer">
+        {{ data.footer }}
+      </div>
     </div>
   </div>
 </template>
@@ -45,25 +49,49 @@ export default {
 <style lang="stylus">
 @import './styles/config.styl'
 
+.home-wrapper
+  width 100%
+  overflow-x hidden;
+
 .home
   padding $navbarHeight 2rem 0
   max-width 960px
   margin 0px auto
+  .hero-bg-mask
+    background: linear-gradient(to right, #2b2e2c 0%, #2b2e2c 60%, transparent 70%, transparent 100%); 
+    position: absolute;
+    left: 0;
+    top: 50px;
+    z-index: -1;
+    width: 100vw;
+    height: 500px;
+  .hero-bg
+    background: url(../public/images/landing_bg.jpg);
+    width: 100vw;
+    height: 500px;
+    position: absolute;
+    left: 0;
+    top: 50px;
+    z-index: -1;
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position-x: right;
+    background-color: #2b2e2c;
   .hero
-    text-align center
+    height 300px
+    padding 80px 0
+    color whitesmoke;
     img
       max-height 280px
       display block
       margin 3rem auto 1.5rem
     h1
       font-size 3rem
-    h1, .description, .action
-      margin 1.8rem auto
     .description
       max-width 35rem
       font-size 1.6rem
       line-height 1.3
-      color lighten($textColor, 40%)
+      color darken(whitesmoke, 40%)
     .action-button
       display inline-block
       font-size 1.2rem
@@ -79,6 +107,7 @@ export default {
   .features
     border-top 1px solid $borderColor
     padding 1.2rem 0
+    margin-bottom: 120px;
     margin-top 2.5rem
     display flex
     flex-wrap wrap
